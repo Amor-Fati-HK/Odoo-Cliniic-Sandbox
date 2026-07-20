062b894d8728b22b09159b42067f8a3bcc0c75a7
Yoda Vert <hanikattipp@gmail.com>
2026-07-20 11:18:10 +0100

    Added the car type class, some fields and the constrain methode to check in case the price is invalide

--- models/vehicule.py
+++ models/vehicule.py
@@ -8,3 +8,38 @@ Car model for he clinic management module
 from odoo import models,fields,api,_
 from odoo.exceptions import ValidationError
 
+class vehicule_type(models.Model):
+    _name="clinic.vehicule.type"
+    _description="Type de vehicule"
+
+    name=fields.Char(string="Nom du Type", required=True)
+
+    _sql_constraints=[
+        ('name_unique', 'UNIQUE(name)', 'Ce type de materiel existe deja !')
+    ]
+
+class vehicule(models.Model):
+    _name="clinic.vehicule"
+    _description="Vehicules de la clinique"
+
+    name=fields.Char(string="Nom / Modele", required=True)
+    description=fields.Text(string="Description")
+    type_id=fields.Many2one('clinic.vehicule.type', String="Type", required=True)
+
+    purchase_date=fields.Date(string="Date d'achat")
+    price=fields.Float(string="Prix d'achat")
+    state=fields.Selection([
+        ('available','Disponible'),
+        ('in_use','En cours dutilisation'),
+        ('repair', 'En maintenance'),
+        ('broken', 'Hors service')
+    ], string="Etat", default='available')
+    
+    
+    @api.constrains('price')
+    def _check_price(self):
+        for s in self:
+            if s.price<0 :
+                raise ValidationError(_("Prix invalide"))
+
+    
